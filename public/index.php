10c94192a09811e5b03b57dbc560cb1c78bab96f
Delphinus_dev <delphine.gobbin@gmail.com>
2019-10-15 19:47:59 +0200

    first commit

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// include'../src/wcs/hello.php';
+// include(SayHello);
+
+require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
+
+use HelloWorld\SayHello;
+
+echo SayHello::world();
